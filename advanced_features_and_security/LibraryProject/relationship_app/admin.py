ee87c66af730aab12ff60d6893714b4937101a0d
MudMoh <mudesir@gmail.com>
2025-11-22 00:53:15 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,18 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Author, Book, Library, Librarian, CustomUser
+from .models import Author, Book, Library, Librarian
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
