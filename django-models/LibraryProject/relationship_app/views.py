f12523e3f05bee78339db846e4034b3689ecca7e
MudMoh <mudesir@gmail.com>
2025-11-22 00:27:55 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,12 +18,11 @@ class BookForm(ModelForm):
 
 def list_books(request):
     books = Book.objects.all()
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list)
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
 class CustomLoginView(LoginView):
